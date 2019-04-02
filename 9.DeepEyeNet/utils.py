f76a272d2b44193c318bd91c7d2f754cf2c8c08b
waynewu6250 <waynewu6250@gmail.com>
2019-04-02 22:18:22 +0800

    Modify

--- 9.DeepEyeNet/utils.py
+++ 9.DeepEyeNet/utils.py
@@ -1,6 +1,13 @@
 import json
 import pickle
 import re
+import numpy as np
+from tqdm import tqdm
+
+from keras.applications import inception_v3, vgg16, vgg19 
+from keras.preprocessing import image
+from keras.models import Model
+
 
 # Load json files
 def load_json(file):
@@ -37,3 +44,44 @@ def text_prepare(text):
         tok = tok[:mxlen]
 
     return " ".join(tok)
+
+
+# Extract image features
+class Extract:
+	def __init__(self, model, dim):
+		if model not in ['inception_v3', 'vgg16', 'vgg19']:
+			raise ValueError('Please specify correct model!!')
+		if model == 'inception_v3':
+			self.model = self.preload(inception_v3.InceptionV3(weights='imagenet'))
+			self.preprocess_input = inception_v3.preprocess_input
+		elif model == 'vgg16':
+			self.model = self.preload(vgg16.VGG16(weights='imagenet'))
+			self.preprocess_input = vgg16.preprocess_input
+		elif model == 'vgg19':
+			self.model = self.preload(vgg19.VGG19(weights='imagenet'))
+			self.preprocess_input = vgg19.preprocess_input
+		self.dim = dim
+	
+	# Preprocess the images
+	def preprocess_img(self, img_path):
+		img = image.load_img(img_path, target_size=(self.dim, self.dim))
+		x = image.img_to_array(img)
+		x = np.expand_dims(x, axis=0)
+		x = self.preprocess_input(x)
+		return x
+	
+	def preload(self, model):
+		new_input = model.input
+		hidden_layer = model.layers[-2].output
+		return Model(new_input, hidden_layer)
+	
+	def extract_features(self, imgs):
+		imgs_features = {}
+		for img in tqdm(imgs):
+			features = self.model.predict(self.preprocess_img(img+'.jpg'))
+			imgs_features[img] = features.squeeze()
+		return imgs_features
+	
+
+
+
