828e9433f314ccd5d176d79e6531f6fe1fb5b9ea
waynewu6250 <waynewu6250@gmail.com>
2019-05-02 23:55:03 +0800

    modify script

--- 5.Movie-bot-pytorch/run.sh
+++ 5.Movie-bot-pytorch/run.sh
@@ -5,17 +5,17 @@ else
     mkdir checkpoints/
 
     curl -L https://www.dropbox.com/s/lybghg3ne2quaw8/memory.pth?dl=1 > checkpoints/memory.pth
-    curl -L https://www.dropbox.com/s/6ncv1zy54lppbsl/memory_chinese.pth?dl=1 > checkpoints/memory_chinese.pth
+    curl -L https://www.dropbox.com/s/lyrc1geco7kr70x/memory_chinese.pth?dl=1 > checkpoints/memory_chinese.pth
     curl -L https://www.dropbox.com/s/vwwnyfv404j0yhq/data.bin?dl=1 > data/data.bin
-    curl -L https://www.dropbox.com/s/hf5rnmw055ogoe8/chinese_data.bin?dl=1 > data/chinese_data.bin
-    curl -L https://www.dropbox.com/s/kn1ol7ilb8dx2cc/xiaohuangji50w_fenciA.conv?dl=1 > data/xiaohuangji50w_fenciA.conv
+    curl -L https://www.dropbox.com/s/2jrm4dl5dijpo1w/chinese_data.bin?dl=1 > data/chinese_data.bin
+    curl -L https://www.dropbox.com/s/a4jgvejqoj83jsw/new_data.conv?dl=1 > data/new_data.conv
 fi
 
 wait
 
 if [ $1 == "train" ]; then
     if [ $2 == "chinese" ]; then
-        python3.6 python main.py train --chinese=True
+        python main.py train --chinese=True
     elif [ $2 == "english" ]; then
         python main.py train --chinese=False
     fi
