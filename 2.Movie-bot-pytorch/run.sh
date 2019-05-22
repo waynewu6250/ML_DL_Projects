798f156ad84a953f4403ba201b3dd3c636363b77
waynewu6250 <waynewu6250@gmail.com>
2019-05-22 11:55:21 +0800

    Modify files

--- 2.Movie-bot-pytorch/run.sh
+++ 2.Movie-bot-pytorch/run.sh
@@ -5,8 +5,10 @@ else
     mkdir checkpoints/
 
     curl -L https://www.dropbox.com/s/lybghg3ne2quaw8/memory.pth?dl=1 > checkpoints/memory.pth
+    curl -L https://www.dropbox.com/s/g99h2e68ei1k7rt/memory_new.pth?dl=1 > checkpoints/memory_new.pth
     curl -L https://www.dropbox.com/s/lyrc1geco7kr70x/memory_chinese.pth?dl=1 > checkpoints/memory_chinese.pth
     curl -L https://www.dropbox.com/s/vwwnyfv404j0yhq/data.bin?dl=1 > data/data.bin
+    curl -L https://www.dropbox.com/s/ti107m6slp1k3ud/data_new.bin?dl=1 > data/data_new.bin
     curl -L https://www.dropbox.com/s/2jrm4dl5dijpo1w/chinese_data.bin?dl=1 > data/chinese_data.bin
     curl -L https://www.dropbox.com/s/a4jgvejqoj83jsw/new_data.conv?dl=1 > data/new_data.conv
     curl -L https://www.dropbox.com/s/5z11n0xsabk0skr/fb_data.bin?dl=1 > data/fb_data.bin
